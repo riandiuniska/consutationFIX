6db96e2f30f13aaa30fed82ac2f224c3392b0a4d
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2022-03-13 15:18:47 +0700

    Add class groups
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

---
+++ db/Groups.php
@@ -0,0 +1,43 @@
+<?php
+
+class Groups {
+    private $group_id;
+    private $group_name;
+    private $db_conn;
+
+    public function __construct() 
+    {
+        require_once("./DbConnect.php");
+        $conn = new DbConnect;
+        $this->db_conn = $conn->connect();
+    }
+
+    public function get_group_id() {
+        return $this->group_id;
+    }
+
+    public function set_group_id($id) {
+        $this->group_id = $id;
+    }
+
+    public function get_group_name() {
+        return $this->group_name;
+    }
+
+    public function set_group_name($name) {
+        $this->group_name = $name;
+    }
+
+    public function get_all_groups() {
+        $stmt = $this->db_conn->prepare("SELECT * FROM groups");
+        try {
+            if($stmt->execute()) {
+                $groupsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+        return $groupsData;
+    }
+}
